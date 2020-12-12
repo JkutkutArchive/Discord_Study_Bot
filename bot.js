7628220dd4d80456f6d1ba29b443980f59e0878c
jkutkut <jorgeregonzalez@gmail.com>
2020-12-12 23:13:52 +0100

    basic communication logic implemented

--- bot.js
+++ bot.js
@@ -10,52 +10,22 @@ client.login(process.env.APY_KEY);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-
-    // Roles
-    // Check if roles were already created
-    // console.log(guild.roles.cache);
-    // guild.roles.create({
-    //     data: {
-    //             name: 'Muted',
-    //             color: 'BLUE',
-    //         },
-    //     reason: 'people with this role can not talk'
-    // });
-    // console.log(guild.roles.fletch('Muted'));
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
-    }
-    else if (msg.content === 'join') {
-        msg.member.voice.channel.join();
-    }
-    else if (msg.content === 'bye') {
-        msg.member.voice.channel.leave();
-    }
-
-    else if (msg.content === 'm') {
-        msg.member.voice.setMute(true);
-        console.log("toggled");
-    }
-
-
-    // Your invokation here, for example your switch/case hook for some command (i.e. '!muteall')
-    // Check if user is in a voice channel:
-    if (msg.content === 'silence'){
-        if (msg.member.voice.channel) {
-            let channel = msg.guild.channels.cache.get(msg.member.voice.channel.id);
-            for (const [memberID, member] of channel.members) {
-                // I added the following if statement to mute everyone but the invoker:
-                // if (member != message.member)
-            
-                // This single line however, nested inside the for loop, should mute everyone in the channel:
-                member.voice.setMute(true);
-                // console.log(member.voice);
-            }
-        } else {
-            msg.reply('You need to join a voice channel first!');
-        }
+    console.log(msg.content);
+    switch(true) {
+        case "ping" == msg.content:
+            msg.reply('Pong!');
+            break;
+        case /\/help/.test(msg.content):
+            msg.reply("Ayuda:\n\t\t\t**/s**: empezar estudiar. \n\t\t\t**/ds**: dejar estudiar.");
+            break;
+        case /\/s/.test(msg.content):
+            msg.reply("Now you are studing");
+            break;
+        case /\/ds/.test(msg.content):
+            msg.reply("You've stop studing");
+            break;
     }
 });
