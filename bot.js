12a5f95a52e4e5966789f3a2d0c120fe8e3db34f
jkutkut <jorgeregonzalez@gmail.com>
2020-12-13 02:04:39 +0100

    Debug commands now avalible. Ready to standar communication and implement logic

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const guild = new Discord.Guild(client);
 var sqlite3 = require('sqlite3').verbose(); // Database
-var db = new sqlite3.Database('./DB/dataBase.db'); //(':memory:');
+var db = new sqlite3.Database(process.env.DB_LOCATION); //(':memory:');
 
 client.login(process.env.APY_KEY);
 
@@ -30,13 +30,21 @@ client.on('message', msg => {
         case /\/ds/.test(msg.content):
             msg.reply("You've stop studing");
             break;
-        case /\/test/.test(msg.content):
+        case /\/t/.test(msg.content):
             msg.reply("debug command detected");
             if (/getData/.test(msg.content)){
                 // getData(db);
                 db.each("SELECT * FROM data;", function(err, row){
                     console.log(row.user + ": " + row.time);
                 })
+            }
+            else if (/exe/.test(msg.content)) {
+                msg.reply("Executing command, see terminal");
+                try {
+                    eval(msg.content.substr(7));
+                } catch (error) {
+                    console.log(error)
+                }
             }
             break;
     }
@@ -45,6 +53,9 @@ client.on('message', msg => {
 db.serialize(function() {
     db.run("CREATE TABLE IF NOT EXISTS data (time BLOB PRIMARY KEY, user TEXT, extra BLOB);");
     db.run("INSERT INTO data (time, user, extra) VALUES (CURRENT_TIMESTAMP, 'System', 'start');");
-
-    console.log("started")
+    console.log("DataBase started");
 });
+
+function prueba(){
+    console.log("msg prueba");
+}
